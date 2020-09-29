0c21fa5bdfecac2a307a88a42152106379254d06
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-09-29 12:14:59 +0530

    Added

---
+++ UC2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UC2_Dailywage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int empCheck = r.Next(0, 2);
+            int IF_PRESENT = 1;
+            int dWage = 20;
+            int empWage, empHours;
+            if (empCheck == IF_PRESENT)
+            {
+                empHours = 8;
+            }
+            else {
+                empHours = 0;
+
+            }
+            empWage = empHours * dWage;
+            Console.WriteLine("Daily Wage : {0}",+empWage);
+        }
+    }
+}
