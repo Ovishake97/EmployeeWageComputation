2274aa0755da306e83fc674e9af9e9a8ce20c246
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-04 19:54:13 +0530

    Added to the master branch

---
+++ Master/Master/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EmployeeWageV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageCalc empWagecalc = new EmpWageCalc();
+            empWagecalc.AddCompanyWage("Dmart", 20, 2, 20,100);
+            empWagecalc.AddCompanyWage("Reliance", 15, 14, 40,90);
+            empWagecalc.ComputeEmpWage();
+            Console.WriteLine("Total wage for Dmart is: " + empWagecalc.GetTotalWage("Dmart"));
+            Console.ReadKey();
+        }
+    }
+
+    public interface IComputeWage
+    {
+        void AddCompanyWage(string company, int dWage,int totalWorkingHours, int WorkingDays, int maxHours);
+
+        void ComputeEmpWage();
+    }
+
+    public interface IComputeWageV2
+    {
+        int GetTotalWage(string company);
+    }
+}
+    
